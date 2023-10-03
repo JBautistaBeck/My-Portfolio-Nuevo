aee51fd6f0a150525c3609c63c076d3badcd6573
JBautistaBeck <juanbautistabeck@gmail.com>
2023-10-03 20:32:29 -0300

    Skills

--- src/views/skills/skills.jsx
+++ src/views/skills/skills.jsx
@@ -1,9 +1,44 @@
+import React, { useState } from 'react';
 import './skills.css';
 
 function Skills() {
+  
+  const [activeTab, setActiveTab] = useState('tech');
+
+  const handleTabChange = (tab) => {
+    setActiveTab(tab);
+  };
+
+  
   return (
-    <div>
-      <h1>Este es el Skills</h1>
+    <div className="skills-container">
+      <div className="tabs">
+
+        <div className={`tab ${activeTab === 'tech' ? 'active' : ''}`} onClick={() => handleTabChange('tech')}> Tech Skills </div>
+
+        <div className={`tab ${activeTab === 'soft' ? 'active' : ''}`} onClick={() => handleTabChange('soft')}> Soft Skills </div>
+
+      </div>
+
+      <div className="tab-content">
+
+        {/* Contenido de Tech Skills */}
+        {activeTab === 'tech' && (
+          <div>
+            <p>Aquí van las habilidades técnicas.</p>
+          </div>
+        )}
+
+
+
+        {/* Contenido de Soft Skills */}
+        {activeTab === 'soft' && (
+          <div>
+            <p>Aquí van las habilidades blandas (soft skills).</p>
+          </div>
+        )}
+
+      </div>
     </div>
   );
 }
