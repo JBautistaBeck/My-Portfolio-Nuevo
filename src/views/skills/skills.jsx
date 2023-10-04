b98fc54eccf220edfacebfaf268bc304a40f4a69
JBautistaBeck <juanbautistabeck@gmail.com>
2023-10-04 12:26:24 -0300

    arreglo bug carrusel

--- src/views/skills/skills.jsx
+++ src/views/skills/skills.jsx
@@ -26,17 +26,17 @@ function Skills() {
         {/* Contenido de Tech Skills */}
         {activeTab === 'tech' && (
           <div>
-            <ul>
-              <li><img src='https://img.icons8.com/color/48/000000/javascript.png'></img> <b>JavaScript</b> </li>
-              <li><img src='https://img.icons8.com/color/48/000000/react-native.png'></img> <b>Reac.js</b> </li>
-              <li><img src='https://img.icons8.com/color/48/000000/nodejs.png'></img> <b>Node.js</b> </li>
-              <li><img src='https://img.icons8.com/color/48/000000/redux.png'></img> <b>Redux</b> </li>
-              <li><img src='https://img.icons8.com/color/48/000000/express.png'></img> <b>Express</b> </li>
-              <li><img src='https://w7.pngwing.com/pngs/441/460/png-transparent-postgresql-plain-wordmark-logo-icon-thumbnail.png'></img> <b>PosgreSQL</b> </li>
-              <li><img src='https://img.icons8.com/color/48/000000/css3.png'></img> <b>CSS</b> </li>
-              <li><img src='https://img.icons8.com/color/48/000000/html-5.png'></img> <b>HTML</b> </li>
-              <li><img src='https://assets.stickpng.com/images/58482ee4cef1014c0b5e4a75.png'></img> <b>Sequelize</b> </li>
-              <li><img src='https://img.icons8.com/color/48/000000/git.png'></img> <b>Git</b> </li>
+            <ul className='ul-skills'>
+              <li className='li-skills'><img className='li-img' src='https://img.icons8.com/color/48/000000/javascript.png'></img> <b>JavaScript</b> </li>
+              <li className='li-skills'><img src='https://img.icons8.com/color/48/000000/nodejs.png'></img> <b>Node.js</b> </li>
+              <li className='li-skills'><img src='https://img.icons8.com/color/48/000000/redux.png'></img> <b>Redux</b> </li>
+              <li className='li-skills'><img src='https://img.icons8.com/color/48/000000/react-native.png'></img> <b>Reac.js</b> </li>
+              <li className='li-skills'><img src='https://img.icons8.com/color/48/000000/express.png'></img> <b>Express</b> </li>
+              <li className='li-skills'><img src='https://w7.pngwing.com/pngs/441/460/png-transparent-postgresql-plain-wordmark-logo-icon-thumbnail.png'></img> <b>PosgreSQL</b> </li>
+              <li className='li-skills'><img src='https://img.icons8.com/color/48/000000/css3.png'></img> <b>CSS</b> </li>
+              <li className='li-skills'><img src='https://img.icons8.com/color/48/000000/html-5.png'></img> <b>HTML</b> </li>
+              <li className='li-skills'><img src='https://assets.stickpng.com/images/58482ee4cef1014c0b5e4a75.png'></img> <b>Sequelize</b> </li>
+              <li className='li-skills'><img src='https://img.icons8.com/color/48/000000/git.png'></img> <b>Git</b> </li>
             </ul>
           </div>
         )}
