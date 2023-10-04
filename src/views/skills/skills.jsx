25209f3f11e825805358ac0b913f52e49e7cefe8
JBautistaBeck <juanbautistabeck@gmail.com>
2023-10-04 09:18:33 -0300

    Iconos Skills

--- src/views/skills/skills.jsx
+++ src/views/skills/skills.jsx
@@ -26,17 +26,16 @@ function Skills() {
         {/* Contenido de Tech Skills */}
         {activeTab === 'tech' && (
           <div>
-            <p>Aquí van las habilidades técnicas.</p>
             <ul>
-              <li>JavaScript</li>
-              <li>Reac.js</li>
-              <li>Node.js</li>
-              <li>Redux</li>
-              <li>Express</li>
-              <li>PosgreSQL</li>
-              <li>Sequelize</li>
-              <li>HTML</li>
-              <li>CSS</li>
+              <li>JavaScript <img src='https://img.icons8.com/color/48/000000/javascript.png'></img></li>
+              <li>Reac.js <img src='https://img.icons8.com/color/48/000000/react-native.png'></img></li>
+              <li>Node.js <img src='https://img.icons8.com/color/48/000000/nodejs.png'></img></li>
+              <li>Redux <img src='https://img.icons8.com/color/48/000000/redux.png'></img></li>
+              <li>Express <img src='https://img.icons8.com/color/48/000000/express.png'></img></li>
+              <li>PosgreSQL <img src='https://img.icons8.com/color/48/000000/javascript.png'></img></li>
+              <li>Sequelize <img src='https://img.icons8.com/color/48/000000/javascript.png'></img></li>
+              <li>HTML <img src='https://img.icons8.com/color/48/000000/html-5.png'></img></li>
+              <li>CSS <img src='https://img.icons8.com/color/48/000000/css3.png'></img></li>
             </ul>
           </div>
         )}
