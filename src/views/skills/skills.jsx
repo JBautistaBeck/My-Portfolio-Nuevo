7edb6e821332a3b3f4bf5107fc54235298c88685
JBautistaBeck <juanbautistabeck@gmail.com>
2023-10-04 10:15:44 -0300

    Finish tech skills

--- src/views/skills/skills.jsx
+++ src/views/skills/skills.jsx
@@ -27,15 +27,16 @@ function Skills() {
         {activeTab === 'tech' && (
           <div>
             <ul>
-              <li>JavaScript <img src='https://img.icons8.com/color/48/000000/javascript.png'></img></li>
-              <li>Reac.js <img src='https://img.icons8.com/color/48/000000/react-native.png'></img></li>
-              <li>Node.js <img src='https://img.icons8.com/color/48/000000/nodejs.png'></img></li>
-              <li>Redux <img src='https://img.icons8.com/color/48/000000/redux.png'></img></li>
-              <li>Express <img src='https://img.icons8.com/color/48/000000/express.png'></img></li>
-              <li>PosgreSQL <img src='https://img.icons8.com/color/48/000000/javascript.png'></img></li>
-              <li>Sequelize <img src='https://img.icons8.com/color/48/000000/javascript.png'></img></li>
-              <li>HTML <img src='https://img.icons8.com/color/48/000000/html-5.png'></img></li>
-              <li>CSS <img src='https://img.icons8.com/color/48/000000/css3.png'></img></li>
+              <li><img src='https://img.icons8.com/color/48/000000/javascript.png'></img> JavaScript </li>
+              <li><img src='https://img.icons8.com/color/48/000000/react-native.png'></img> Reac.js </li>
+              <li><img src='https://img.icons8.com/color/48/000000/nodejs.png'></img> Node.js </li>
+              <li><img src='https://img.icons8.com/color/48/000000/redux.png'></img> Redux </li>
+              <li><img src='https://img.icons8.com/color/48/000000/express.png'></img> Express </li>
+              <li><img src='https://w7.pngwing.com/pngs/441/460/png-transparent-postgresql-plain-wordmark-logo-icon-thumbnail.png'></img> PosgreSQL </li>
+              <li><img src='https://img.icons8.com/color/48/000000/css3.png'></img> CSS </li>
+              <li><img src='https://img.icons8.com/color/48/000000/html-5.png'></img> HTML </li>
+              <li><img src='https://assets.stickpng.com/images/58482ee4cef1014c0b5e4a75.png'></img> Sequelize </li>
+              <li><img src='https://img.icons8.com/color/48/000000/git.png'></img> Git </li>
             </ul>
           </div>
         )}
